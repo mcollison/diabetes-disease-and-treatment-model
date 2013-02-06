0c900d0909bbb57613cbcba421a2d32e274f949b
Matt Collison <m.g.collison@ncl.ac.uk>
2013-02-06 15:20:58 +0000

    Daily commit.

--- metaboliteAnovaStats.m
+++ metaboliteAnovaStats.m
@@ -1,7 +1,11 @@
-function [anovaAllGroups, anovaTreatmentGroups, anovaTestGroups] = metaboliteAnovaStats(metabolite, conditionStr, graphswitch)
+function [anovaAllGroups] = metaboliteAnovaStats(metabolite, conditionStr, graphswitch)
+
+% for i=1:length(metabolite(:,1))
+%     [anovaAllGroups{i,1}, anovaAllGroups{i,2}, anovaAllGroups{i,3}] = anova1(metabolite(i,:), conditionStr, graphswitch);
+%     [anovaTreatmentGroups{i,1},anovaTreatmentGroups{i,2},anovaTreatmentGroups{i,3}] = anova1(metabolite(i, 11:59), conditionStr, graphswitch);
+%     [anovaTestGroups{i,1},anovaTestGroups{i,2},anovaTestGroups{i,3}] = anova1(metabolite(i, [11:29 50:59]), conditionStr, graphswitch);
+% end
 
 for i=1:length(metabolite(:,1))
-    [anovaAllGroups{i,1}, anovaAllGroups{i,2}, anovaAllGroups{i,3}] = anova1(metabolite(i,:), conditionStr, graphswitch);
-    [anovaTreatmentGroups{i,1},anovaTreatmentGroups{i,2},anovaTreatmentGroups{i,3}] = anova1(metabolite(i, 11:59), conditionStr, graphswitch);
-    [anovaTestGroups{i,1},anovaTestGroups{i,2},anovaTestGroups{i,3}] = anova1(metabolite(i, [11:29 50:59]), conditionStr, graphswitch);
+    [anovaAllGroups{i,1}, anovaAllGroups{i,2}, anovaAllGroups{i,3}] = kruskalwallis(metabolite(i,:), conditionStr, graphswitch);
 end
